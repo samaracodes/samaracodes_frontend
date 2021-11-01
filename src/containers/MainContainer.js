ea4455ea4b715379f7cf1c73be9fca69fb873e02
Sam <samara.amanda01@gmail.com>
2021-11-01 10:40:27 -0400

    added some content to menu bar dropdown

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { Component } from 'react';
 import { Switch, Route } from 'react-router'
 import Home from '../components/Home'
-import About from '../components/About'
-import Contact from '../components/Contact'
-import MyProjects from '../components/MyProjects'
 import NavBar from '../components/NavBar'
 
 
@@ -15,9 +12,6 @@ class MainContainer extends Component {
                 <NavBar/>
                 <Switch>
                     <Route exact path="/" component={Home} />
-                    <Route exact path="/projects" component={MyProjects} />
-                    <Route exact path="/about" component={About} />
-                    <Route exact path="/contact" component={Contact} />
                 </Switch>
             </>
         )
